01ebe8467f3205d173397e32b5e21b209537cf9e
Jared Buchanan <jaredb@phxformulations.com>
2021-07-29 16:26:00 -0700

    updates

--- RedGrim.Mobile/RedGrim.Mobile/Controls/MapControl.xaml.cs
+++ RedGrim.Mobile/RedGrim.Mobile/Controls/MapControl.xaml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-//using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps;
 
 namespace RedGrim.Mobile.Controls
 {
@@ -22,65 +22,65 @@ namespace RedGrim.Mobile.Controls
             //CreateMap();
         }
 
-        //public void CreateMap()
-        //{
-        //    GetCurrentCoordinates();
-        //    Position startingPoint = new Position(currentLat, currentLong);
-        //    MapSpan span = new MapSpan(startingPoint, .01, .01);
+        public void CreateMap()
+        {
+            GetCurrentCoordinates();
+            Position startingPoint = new Position(currentLat, currentLong);
+            MapSpan span = new MapSpan(startingPoint, .01, .01);
 
-        //    mainMap = new Xamarin.Forms.Maps.Map(span);
-        //    mainMap.MapType = MapType.Satellite;
-            
+            mainMap = new Xamarin.Forms.Maps.Map(span);
+            mainMap.MapType = MapType.Satellite;
 
-        //    mapLayout.Children.Add(mainMap);
-            
-        //}
 
-        //private async void GetCurrentCoordinates()
-        //{
-        //    Location currentLocation = await Geolocation.GetLastKnownLocationAsync();
-        //    currentLat = currentLocation.Latitude;
-        //    currentLong = currentLocation.Longitude;
-        //}
-        
+            mapLayout.Children.Add(mainMap);
 
-        //private async void PanToCurrentLocation()
-        //{
-        //    try
-        //    {
-        //        GetCurrentCoordinates();
-        //        if (currentLat != null && currentLong != null)
-        //        {
-        //            Position pos = new Position(currentLat, currentLong);
-        //            MapSpan mspan = new MapSpan(pos, .01, .01);
-        //            mainMap.MoveToRegion(mspan);
+        }
 
-        //            Pin currentPin = new Pin();
-        //            currentPin.Position = pos;
-        //            currentPin.Label = "^";
-        //            currentPin.StyleId = "current";
-        //            currentPin.Type = PinType.Generic;
+        private async void GetCurrentCoordinates()
+        {
+            Location currentLocation = await Geolocation.GetLastKnownLocationAsync();
+            currentLat = currentLocation.Latitude;
+            currentLong = currentLocation.Longitude;
+        }
 
-        //            mainMap.Pins.Add(currentPin);
-        //        }
-        //    }
-        //    catch (FeatureNotSupportedException fnsEx)
-        //    {
-        //        // Handle not supported on device exception
-        //    }
-        //    catch (FeatureNotEnabledException fneEx)
-        //    {
-        //        // Handle not enabled on device exception
-        //    }
-        //    catch (PermissionException pEx)
-        //    {
-        //        // Handle permission exception
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Unable to get location
-        //    }
-        //}
+
+        private async void PanToCurrentLocation()
+        {
+            try
+            {
+                GetCurrentCoordinates();
+                if (currentLat != null && currentLong != null)
+                {
+                    Position pos = new Position(currentLat, currentLong);
+                    MapSpan mspan = new MapSpan(pos, .01, .01);
+                    mainMap.MoveToRegion(mspan);
+
+                    Pin currentPin = new Pin();
+                    currentPin.Position = pos;
+                    currentPin.Label = "^";
+                    currentPin.StyleId = "current";
+                    currentPin.Type = PinType.Generic;
+
+                    mainMap.Pins.Add(currentPin);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
 
         #region Map Button Handlers
 
